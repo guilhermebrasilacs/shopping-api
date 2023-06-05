aecf95542827c91e038a3c9ded26786b594f9118
Dev01 <guilhermebrasil.acs@outlook.com>
2023-06-05 11:06:51 -0300

    Shop Repository

---
+++ src/main/java/com/project4/ACSchapter6/shoppingapi/repository/ShopRepository.java
@@ -0,0 +1,17 @@
+package com.project4.ACSchapter6.shoppingapi.repository;
+
+import java.time.LocalDateTime;
+import	java.util.Date;
+import	java.util.List;
+
+import	org.springframework.data.jpa.repository.JpaRepository;
+import	org.springframework.stereotype.Repository;
+
+import	com.project4.ACSchapter6.shoppingapi.model.Shop;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long>{
+    public List<Shop> findAllByUserIdentifier(String userIdentifier);
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+    public List<Shop> findAllByDateGreaterThan(LocalDateTime date);
+}
